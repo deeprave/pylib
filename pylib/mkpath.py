b02f9d824bc79e4f0fb356a17622cbe041742156
David Nugent <davidn@uniquode.io>
2019-03-21 16:21:55 +1100

    Don't complain if path is none or empty

--- pylib/mkpath.py
+++ pylib/mkpath.py
@@ -6,10 +6,11 @@ import sys
 
 
 def mkpath(path, mode=0o755):
-    try:
-        os.makedirs(path, mode)
-        logging.debug("Created folder: {}".format(path))
-    except (OSError, IOError):
-        exc = sys.exc_info()
-        if exc[1].errno != errno.EEXIST:
-            raise
+    if path:
+        try:
+            os.makedirs(path, mode)
+            logging.debug("Created folder: {}".format(path))
+        except (OSError, IOError):
+            exc = sys.exc_info()
+            if exc[1].errno != errno.EEXIST:
+                raise
