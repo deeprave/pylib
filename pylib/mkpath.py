81bf717ed407d50cc92e7c8a45bb85f03a59e4e1
David Nugent <david.nugent@unico.com.au>
2019-06-01 12:15:39 +1000

    Allow suppression of log message when creating a directory

--- pylib/mkpath.py
+++ pylib/mkpath.py
@@ -5,11 +5,12 @@ import os
 import sys
 
 
-def mkpath(path, mode=0o755):
+def mkpath(path, mode=0o755, log_create=False):
     if path:
         try:
             os.makedirs(path, mode)
-            logging.debug("Created folder: {}".format(path))
+            if log_create:
+                logging.debug("Created folder: {}".format(path))
         except (OSError, IOError):
             exc = sys.exc_info()
             if exc[1].errno != errno.EEXIST:
